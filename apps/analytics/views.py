b35d8210bcc6d71a01f296b2c7282723f7b687cc
Grigoriy1209 <grigoriyv1209@gmail.com>
2024-11-28 17:10:44 +0200

    1

--- apps/analytics/views.py
+++ apps/analytics/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -19,7 +20,8 @@ class ViewsCountApiView(APIView):
 class ViewsByDaysApiView(APIView):
     permission_classes = (IsPremiumSeller,)
 
-    def get(self, request, *args, **kwargs):
+
+def get(self, request, *args, **kwargs):
         user = request.user
         days = int(kwargs.get('days', ))
         views = CarsModel.objects.views_count_days(days=days, user=user)
